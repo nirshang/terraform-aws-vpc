5b9c04dfb1f508debf697d071dca07f59d55e511
nirshang <ngodugunuri@gmail.com>
2025-02-06 12:25:00 -0700

    vpc module

---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "project_name"{
+
+}
+
+variable "environment"{
+
+}
+
+variable "vpc_cidr" {
+
+}
+
+variable "enable_dns_hostnames" {
+    default = true
+}
+
+# mandatoryt
+variable "common_tags"{
+    type = map
+    #default = {}
+}
